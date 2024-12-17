0f968ad69ad416cbedeae33fdbd9bcbd1f0972a0
임선오 <vhtmxj154@naver.com>
2024-12-17 21:16:52 +0900

    update: account_model add

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,7 +1,30 @@
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-from django.contrib.auth.models import AbstractBaseUser
+from .validators import UnicodeUsernameValidator
+
+username_validator = UnicodeUsernameValidator()
+
 
 class User(AbstractBaseUser):
-    nickname = models.CharField(max_length=50, db_column='nickname', help_text='nickname', null=False, blank=False)
-    name = models.CharField(max_length=50, db_column='name', help_text='name', null=False, blank=False)
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text="필수 입력 사항입니다. 150자 이하. 문자, 숫자 및 @/./+/-/_만 가능합니다.",
+        validators=[username_validator],
+        error_messages="unique":"중복된 아이디 입니다.",
+        null=False,
+        blank=False,
+        db_column="userid",
+    )
+    nickname = models.CharField(
+        max_length=50,
+        db_column="nickname",
+        help_text="nickname",
+        null=False,
+        blank=False,
+    )
+    name = models.CharField(
+        max_length=50, db_column="name", help_text="실제이름", null=False, blank=False
+    )
+    
---
+++ backend/accounts/validators.py
@@ -0,0 +1,15 @@
+import re
+
+from django.core import validators
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
+
+
+@deconstructible
+class UnicodeUsernameValidator(validators.RegexValidator):
+    regex = r"^[\w.@+-]+\Z"
+    message = _(
+        "유효한 사용자 아이디를 입력합니다. 이 값에는 문자, "
+        "숫자 및 @/./+/-/_ 문자만 포함할 수 있습니다."
+    )
+    flags = 0
