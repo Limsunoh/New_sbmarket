e655a3ffabcf456208583a25bfd4f9b5cfbb6226
임선오 <vhtmxj154@naver.com>
2025-01-20 18:27:43 +0900

    add: Frontend Review url,view 작성

--- frontend/reviews/views.py
+++ frontend/reviews/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.views.generic import TemplateView
+
+from backend.reviews.models import CHECKLIST_OPTIONS
+
+
+class ReviewCreateView(TemplateView):
+    template_name = "review_create.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["product_id"] = kwargs.get("product_id")
+        context["checklist_options"] = CHECKLIST_OPTIONS
+        return context
